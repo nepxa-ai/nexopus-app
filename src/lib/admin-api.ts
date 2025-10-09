2e12deb87f62bb9197ce4069e76a727e16634732
andres suarez <andres.suarez@nepxa.ai>
2025-10-08 23:04:12 -0500

    Se ajusta admin-api ts para validar dev - prod

--- src/lib/admin-api.ts
+++ src/lib/admin-api.ts
@@ -1,4 +1,5 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "/api";
+
 
 export type User = {
   id: number;
